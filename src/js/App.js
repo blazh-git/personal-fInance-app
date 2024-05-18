b3f026ee5c04814299fec9be410539e7379e1bb8
blazh-git <blaz@modrijan-izobrazevanje.si>
2024-05-18 21:17:12 +0200

    Added footer

--- src/js/App.js
+++ src/js/App.js
@@ -6,7 +6,7 @@ import Expenses from "./Components/Expenses";
 const App = () => {
     const [totalIncome, setTotalIncome] = useState(0);
     const [totalExpenses, setTotalExpenses] = useState(0);
-    return(
+    return(<>
         <div className="main-cont">
             <div className="center-cont">
                 <header>
@@ -19,7 +19,10 @@ const App = () => {
                 </div>
             </div>
         </div>
-    )
+        <footer>
+            <p className="copyright">Developed and designed by <a href="https://github.com/blazh-git" target="_blank">blazh</a></p>
+        </footer>
+    </>)
 }
 
 export default App;
